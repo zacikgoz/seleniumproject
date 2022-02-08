8bf753af4bfcca4aac0e4db2af615ed90e3568de
Zuhal Acikgoz <zacikgoz@hotmail.com>
2022-02-08 10:49:42 -0600

    day - Utilities

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
@@ -0,0 +1,45 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_locators_getText {
+    public static void main(String[] args) {
+        //1: NextBaseCRM, locators and getText() practice 1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com");
+
+        //3- Enter incorrect username: “incorrect”
+        //driver.findElement(By.className("login-inp")).sendKeys("incorrect"); // we can not reuse it
+        WebElement inputUsername = driver.findElement(By.className("login-inp"));
+        inputUsername.sendKeys("incorrect");
+
+        //4- Enter incorrect password: “incorrect”
+        WebElement inputPassword = driver.findElement(By.name("USER_PASSWORD"));
+        inputPassword.sendKeys("incorrect");
+
+        //5- Click to login button
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
+
+        //6- Verify error message text is as expected:
+        // Expected: Incorrect login or password
+        WebElement error = driver.findElement(By.className("errortext"));
+        String expectedErrorMessage = "Incorrect login or password";
+        String actualErrorMessage = error.getText();
+
+        if(actualErrorMessage.equals(expectedErrorMessage)){
+            System.out.println("Error message verification passed");
+        }else{
+            System.out.println("Error message verification failed");
+        }
+    }
+
+}
+/*PS: Inspect and decide which locator you should be using to locate web elements.*/
